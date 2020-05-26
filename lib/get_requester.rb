bcd8e2153eb9cac0de90003cac1bfeb0902df13b
Lon Bitner <lonbitner@gmail.com>
2020-05-26 20:01:50 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -13,7 +13,7 @@ end
 url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json."
 
 def get_response_body
-uri = URI.parse(url)
+uri = URI.parse(@url)
 response = Net::HTTP.get_respones(uri)
 response.body
 end
