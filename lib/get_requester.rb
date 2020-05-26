bb348d5bdb0482f93d60656f6c3d69650a0ceb86
Lon Bitner <lonbitner@gmail.com>
2020-05-26 19:41:43 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,5 +1,2 @@
-# Write your code here
-class GetRequester
-  
-  
-end
+get_requester = GetRequester.new('https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json')
+get_requester.parse_json
